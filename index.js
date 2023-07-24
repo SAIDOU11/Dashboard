575a460b7dcf85d5bc2aa97b89306f4c6543607d
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-07-24 18:06:26 +0200

    dashboard

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const imgName = document.getElementById("crypto-top");
+const crypto = document.getElementById("crypto");
+
 fetch(
   "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
 )
@@ -10,11 +13,7 @@ fetch(
   .catch((err) => {
     document.body.style.backgroundImage = `url("https://images.unsplash.com/photo-1537210249814-b9a10a161ae4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2OTAwODY4MzV8&ixlib=rb-4.0.3&q=80&w=1080")`;
     document.getElementById("author").innerHTML = "By: Felix Mitierterme";
-    // Report the errror to some kind of service
   });
-
-const imgName = document.getElementById("crypto-top");
-const crypto = document.getElementById("crypto");
 
 fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
   .then((res) => {
@@ -42,25 +41,33 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
     console.error(err);
   });
 
-// function getCurrentTime() {
-//   const date = new Date();
-//   let hourNow = date.toLocaleTimeString("fr-FR", { timeStyle: "short" });
-//   document.getElementById("time").innerHTML = hourNow;
-// }
-
-// setInterval(getCurrentTime, 1000);
-
-function findMyCoordinates() {
-  if (navigator.geolocation) {
-    console.log(
-      "?? ",
-      navigator.geolocation.getCurrentPosition((position) =>
-        console.log(position)
-      )
-    );
-  } else {
-    alert("Geolocalisation is not supported by your browser.");
-  }
+function getCurrentTime() {
+  const date = new Date();
+  let hourNow = date.toLocaleTimeString("fr-FR", { timeStyle: "short" });
+  document.getElementById("time").innerHTML = hourNow;
 }
 
-findMyCoordinates();
+setInterval(getCurrentTime, 1000);
+
+navigator.geolocation.getCurrentPosition((position) => {
+  fetch(
+    `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw Error("Weather data not available");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      document.getElementById("img-temp").innerHTML = `
+      <img src=${iconUrl} />
+      <p>${data.main.temp}°</p>
+    
+      `;
+      document.getElementById("temp").innerHTML = `<p>${data.name}</p>`;
+    })
+    .catch((err) => console.error(err));
+});
